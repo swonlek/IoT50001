e5fc87344bb663da44a68554d686bc52ada3d5ca
swonlek <chonglokswen97@hotmail.com>
2017-11-20 13:34:28 +0800

    1. app > build.gradle updated to increase jumbo dex 2. filled the MainActivity with codes from the part1 pdf

---
+++ Documents/Skewl/Term_4/50-001/Week-11/UDOOTestStart/app/src/main/java/xbee/udootest/MainActivity.java
@@ -0,0 +1,196 @@
+package xbee.udootest;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.usb.UsbManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import me.palazzetti.adktoolkit.AdkManager;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.lazy.IBk;
+import weka.core.Attribute;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
+import wlsvm.WLSVM;
+
+
+
+
+
+
+public class MainActivity extends Activity{
+
+//	private static final String TAG = "UDOO_AndroidADKFULL";	 
+
+    private AdkManager mAdkManager;
+
+    private ToggleButton buttonLED;
+    private TextView distance;
+    private TextView pulse;
+    private TextView position;
+
+    private AdkReadTask mAdkReadTask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAdkManager = new AdkManager((UsbManager) getSystemService(Context.USB_SERVICE));
+
+//		register a BroadcastReceiver to catch UsbManager.ACTION_USB_ACCESSORY_DETACHED action
+        registerReceiver(mAdkManager.getUsbReceiver(), mAdkManager.getDetachedFilter());
+
+        buttonLED = (ToggleButton) findViewById(R.id.toggleButtonLED);
+        distance  = (TextView) findViewById(R.id.textView_distance);
+        pulse  = (TextView) findViewById(R.id.textView_pulse);
+        position  = (TextView) findViewById(R.id.textView_position);
+
+        /* Open training data set file */
+        File root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File f = new File(root, "iris_train.arff");
+
+        BufferedReader inputReader = null;
+        try {
+            inputReader = readFile(f);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        /* Build KNN model */
+        Instances data = null;
+        try {
+            data = new Instances(inputReader);
+            data.setClassIndex(data.numAttributes()-1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        // lazy method in weka
+        Classifier ibk = new IBk();
+        try {
+            ibk.buildClassifier(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        /* Load test dataset, perform classification */
+        f = new File(root, "iris_test.arff");
+        inputReader = null;
+        try {
+            inputReader = readFile(f);
+            Instances test =  new Instances(inputReader);
+            test.setClassIndex(test.numAttributes()-1);
+
+            for (int i = 0; i < test.numInstances(); i++){
+                double pred =  ibk.classifyInstance(test.instance(i));
+                double act = test.instance(i).classValue();
+                // TODO: compare  prediction results with actual class label
+
+            }
+            // TODO: report number of correct and incorrect classifications
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private BufferedReader readFile(File f) throws FileNotFoundException {
+        return new BufferedReader(new FileReader(f));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mAdkManager.open();
+
+        mAdkReadTask = new AdkReadTask();
+        mAdkReadTask.execute();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mAdkManager.close();
+
+        mAdkReadTask.pause();
+        mAdkReadTask = null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mAdkManager.getUsbReceiver());
+    }
+
+    // ToggleButton method - send message to SAM3X
+    public void blinkLED(View v){
+        if (buttonLED.isChecked()) {
+            // writeSerial() allows you to write a single char or a String object.
+            mAdkManager.writeSerial("1");
+        } else {
+            mAdkManager.writeSerial("0");
+        }
+    }
+
+    /*
+     * We put the readSerial() method in an AsyncTask to run the
+     * continuous read task out of the UI main thread
+     */
+    private class AdkReadTask extends AsyncTask<Void, String, Void> {
+
+        private boolean running = true;
+
+        public void pause(){
+            running = false;
+        }
+
+        protected Void doInBackground(Void... params) {
+//	    	Log.i("ADK demo bi", "start adkreadtask");
+            while(running) {
+                publishProgress(mAdkManager.readSerial()) ;
+            }
+            return null;
+        }
+
+        protected void onProgressUpdate(String... progress) {
+
+            float pulseRate= (int)progress[0].charAt(0);
+            float oxygenLvl= (int)progress[0].charAt(1);
+            float pos= (int)progress[0].charAt(2);
+            int max = 255;
+            if (pulseRate>max) pulseRate=max;
+            if (oxygenLvl>max) oxygenLvl=max;
+            if (pos>max) pos=max;
+
+//            DecimalFormat df = new DecimalFormat("#.#");
+            distance.setText(pulseRate + " (bpm)");
+            pulse.setText(oxygenLvl + " (pct)");
+            position.setText(pos + "");
+        }
+    }
+
+
+
+}
